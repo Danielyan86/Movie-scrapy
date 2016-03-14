8e570d1139810e0136a1e090d47b0df8be40d5d8
xiaodong.yan <xiaodong.yan@tubemogul.com>
2016-03-14 10:44:22 +0800

    update code

--- movie/movie/pipelines.py
+++ movie/movie/pipelines.py
@@ -7,27 +7,27 @@
 from pymongo import MongoClient
 import re
 
-client = MongoClient()
-db = client.movie_db
-movie_doc_collection = client.movie_db.movie_doc
-
-year_pattern = re.compile(r'\((\d+)\)')
-name_pattern = re.compile(r'(.*)\(')
-
 
 class MoviePipeline(object):
+    year_pattern = re.compile(r'\((\d+)\)')
+    name_pattern = re.compile(r'(.*)\(')
+
+    def __init__(self):
+        client = MongoClient()
+        # db = client.movie_db
+        self.movie_doc_collection = client.movie_db.movie_doc
+
     def process_item(self, item, spider):
 
-        if not movie_doc_collection.find({'mtime_url': item['mtime_url']}).count():
+        if not self.movie_doc_collection.find({'mtime_url': item['mtime_url']}).count():
             print("insert the moive into db")
-            m = year_pattern.search(item['movie_name'])
-            if m:
-
-                year = m.groups()[0]
-            m = name_pattern.search(item['movie_name'])
-            if m:
-                name = m.groups()[0]
-                result = movie_doc_collection.insert_one({"moive": name, 'year': year, 'mtime_url': item['mtime_url']})
-                print(result.inserted_id)
+            m = self.year_pattern.search(item['movie_name'])
+            year = m.groups()[0] if m else None
+            m = self.name_pattern.search(item['movie_name'])
+            name = m.groups()[0] if m else item['movie_name']
+            result = self.movie_doc_collection.insert_one(
+                {"moive": name, 'year': year, 'mtime_url': item['mtime_url']})
+            print(result.inserted_id)
         else:
             print("movie is existing in db, jump it")
+        return item
