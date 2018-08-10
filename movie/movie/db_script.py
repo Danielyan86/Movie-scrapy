f9ad7f677da0bbf2fa32ecc528333d0b5d2bb698
Danielyan86 <516495459@qq.com>
2018-08-10 14:09:35 +0800

    Update db script

--- movie/movie/db_script.py
+++ movie/movie/db_script.py
@@ -4,25 +4,31 @@ import re
 client = MongoClient()
 movie_doc_obj = client.movie_db.movie_doc
 
-year_pattern = re.compile(r'\((\d+)\)')
+year_pattern = re.compile(r'(\d+)')
 name_pattern = re.compile(r'(.*)\(')
 
 
 def fetch_movie_year_from_name():
     for item in movie_doc_obj.find():
 
-        m = year_pattern.search(item['movie'])
+        m = year_pattern.search(item['year'])
         if m:
             year = int(m.groups()[0])
         else:
             print("not exist number, jump it")
             continue
 
-        movie_doc_obj.update_one({'movie_name': item['movie_name']},
+        movie_doc_obj.update_one({'movie_name': item['movie']},
                                  {'$set': {'year': year,
-                                           'movie_name': item['movie_name'].replace(m.groups()[0],
-                                                                                    "")}})  # update one collection with specified name
+                                           'movie_name': item['movie'].replace(m.groups()[0],
+                                                                               "")}})  # update one collection with specified name
+
+# 查询某个年代的电影
+def get_movie_with_name(year):
+    for item in movie_doc_obj.find():
+        if item['year'] == year:
+            print(item)
 
 
 if __name__ == '__main__':
-    fetch_movie_year_from_name()
+    get_movie_with_name("2004")
